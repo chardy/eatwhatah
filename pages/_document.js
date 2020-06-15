9f7e4d7b5f75105d232135ad0baa7349d7445e83
Chardy <chardy@gmail.com>
2020-06-15 21:36:19 +0800

    adjust og-image

--- pages/_document.js
+++ pages/_document.js
@@ -9,12 +9,12 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Mansalva&display=swap"/>
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width,initial-scale=1" />
-          <meta key="og-sitename" property="og:site_name" content="eatwhatah.com" />
+          <meta key="og-sitename" property="og:site_name" content="Eat what ah?" />
           <meta key="og-type" property="og:type" content="website" />
           <meta key="og-url" property="og:url" content="https://eatwhatah.com" />
           <meta key="og-locale" property="og:locale" content="en_US" />
           <meta key="og-description" property="og:description" content="What do you feel like eating?" />
-          <meta key="og-title" property="og:title" content="eatwhatah?" />
+          <meta key="og-title" property="og:title" content="Eat what ah?" />
           <meta key="og-image" property="og:image" content="/og-image.jpg" />
           <meta key="meta-description" name="description" content="What do you feel like eating?" />
           <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
